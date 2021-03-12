e6c67b8dc21bf603010aca221f30ecacd2ae7fc5
DESKTOP-FD4CQOG\Gizmo <andrea_v@post.cz>
2021-03-12 15:45:54 +0100

    ukol 01

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // tady je místo pro náš program
 
-document.querySelector("#vysledek").innerHTML = secti(4, 5);
+//document.querySelector("#vysledek").innerHTML = secti(4, 5);
 
 /**
  * Sečte dvě čísla na vstupu a vrátí výsledek
@@ -13,9 +13,30 @@ function secti(a, b) {
   return c;
 }
 
+
+//------------------------mé řešení-------------------------------------------------
+
 /**
  * Upozorní uživatele při spuštění.
  */
 function upozorni() {
   alert("Gratulace, právě jsi spustila tuto funkci!");
+  console.log("Gratulace, právě jsi spustila tuto funkci!");
+
+  let prepisCtverecek = document.getElementsByClassName('ctverecek')[0];
+  prepisCtverecek.innerHTML = 'Gratulace, právě jsi spustila tuto funkci!';
+  prepisCtverecek.style.width = '600px';
 }
+
+//změna barvy čtverečku
+function zmenBarvu() {
+  document.querySelector('.ctverecek').style.backgroundColor = '#006400';
+}
+
+//zobraz výsledek až po zmáčknutí tlačítka
+function zobrazVysledek() {
+  document.querySelector("#vysledek").innerHTML = secti(4, 5);
+}
+
+
+
